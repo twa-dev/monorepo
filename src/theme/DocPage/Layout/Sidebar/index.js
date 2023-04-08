13b570bd610a058e38d9c22caf1d1504eb969d74
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-04-08 18:15:27 +0500

    chore(images): replace old images with new

--- src/theme/DocPage/Layout/Sidebar/index.js
+++ src/theme/DocPage/Layout/Sidebar/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Sidebar from '@theme-original/DocPage/Layout/Sidebar';
 import styles from './styles.module.scss';
-import Logo from './logo.svg';
+import logoUrl from './logo.png';
 
 export default function SidebarWrapper(props) {
   return (
     <div className={styles.root}>
       <div className={styles.sticky}>
         <div className={styles.logoWrapper}>
-          <Logo/>
+          <img width={32} height={32} src={logoUrl} alt={'twa dev logo'}/>
           <span className={styles.logoText}>Web Apps</span>
         </div>
         <Sidebar {...props} />
