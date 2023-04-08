188df95777b0f3a476795d103522ced7b11ae8df
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-04-08 17:40:02 +0500

    feat(initial): initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {DefaultLayout} from '@site/src/components/home';
+
+/**
+ * Index entry for whole documentation.
+ * @constructor
+ */
+export default function Home(): JSX.Element {
+  return <DefaultLayout/>;
+}
---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,12 @@
+import MDXComponents from '@theme-original/MDXComponents';
+import NpmInstall from '../components/docs/NpmInstall';
+import LibraryBadges from '../components/docs/LibraryBadges';
+import NpmLink from '../components/docs/NpmLink';
+
+export default {
+  // Re-use the default mapping
+  ...MDXComponents,
+  NpmInstall,
+  LibraryBadges,
+  NpmLink,
+};
